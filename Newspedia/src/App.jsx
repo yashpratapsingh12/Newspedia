8c81c96b559c61e540193a771801bf7c5797c195
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2024-07-23 04:23:30 -0400

    adding components

--- Newspedia/src/App.jsx
+++ Newspedia/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+<button type="button" className="btn btn-primary">Primary</button>
+<button type="button" className="btn btn-secondary">Secondary</button>
+<button type="button" className="btn btn-success">Success</button>
+<button type="button" className="btn btn-danger">Danger</button>
+<button type="button" className="btn btn-warning">Warning</button>
+<button type="button" className="btn btn-info">Info</button>
+<button type="button" classNameName="btn btn-light">Light</button>
+<button type="button" className="btn btn-dark">Dark</button>
+
+<button type="button" className="btn btn-link">Link</button>
+      
+    </div>
   )
 }
 
